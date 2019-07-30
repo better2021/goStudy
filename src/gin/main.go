ffe624ee15cc0d6bc436b0e2dc7cff18aa5da26a
feiyu <feiyu@tg10010.com>
2019-07-30 19:59:27 +0700

    'gin'

--- src/gin/main.go
+++ src/gin/main.go
@@ -1,52 +1,61 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 // GET函数
-func hello(c * gin.Context){
+func hello(c *gin.Context) {
 	c.String(http.StatusOK, "hello gin Get")
 }
 
 // POST函数
-func helloPost(c * gin.Context){
-	c.String(http.StatusOK,"gin Post")
+func helloPost(c *gin.Context) {
+	c.String(http.StatusOK, "gin Post")
 }
 
 // 提取请求中的参数
-func fetchId(c * gin.Context){
+func fetchId(c *gin.Context) {
 	id := c.Param("id")
-	c.String(http.StatusOK,fmt.Sprintln("id is",id))
+	c.String(http.StatusOK, fmt.Sprintln("id is", id))
 }
 
 // 组路由
-func action1(c*gin.Context){
-	c.String(http.StatusOK,"action1")
+func action1(c *gin.Context) {
+	c.String(http.StatusOK, "action1")
 }
 
-func action2(c*gin.Context){
-	c.String(http.StatusOK,"action2")
+func action2(c *gin.Context) {
+	c.String(http.StatusOK, "action2")
 }
 
-func main(){
+func homePage(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello,let is go",
+	})
+}
+
+func main() {
 	fmt.Println("hello gin")
 	router := gin.Default()
 
+	router.GET("/", homePage)
+
 	// RESTful 路由
-	router.GET("/getGin",hello)
-	router.POST("/postGin",helloPost)
+	router.GET("/getGin", hello)
+	router.POST("/postGin", helloPost)
 
 	// 提取请求中的参数
-	router.GET("/param/:id",fetchId)
+	router.GET("/param/:id", fetchId)
 
 	// 组路由
 	group1 := router.Group("/go1")
 	{
-		group1.GET("/action1",action1)
-		group1.GET("/action2",action2)
+		group1.GET("/action1", action1)
+		group1.GET("/action2", action2)
 	}
 
 	// 启动服务
